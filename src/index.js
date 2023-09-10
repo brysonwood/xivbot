50a9a47c8c1241fdac6737e5e03359c1bae3533e
Bryson Wood <mail@brysonwood.com>
2023-09-09 23:22:01 -0700

    Added XIVAPI support.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,46 @@
 require('dotenv').config();
+const axios = require('axios');
+
+const xivApi = 'https://xivapi.com';
+
 const {Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions, messageLink} = require('discord.js');
+
 const client = new Client({intents: [
   GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent
 ]});
 
 const prefix = '!';
 
+// Testing vars
+const xivName = "Kage Dragneel";
+const xivServer = "Cactuar";
+
 
 client.on("ready", () => {
   console.log("Bot is ready!");
-  client.user.setActivity(`Hello.`);
 });
 
 client.on("messageCreate", msg  => {
   if (!msg.content.startsWith(prefix) || msg.author.bot) return;
     
   const args = msg.content.slice(prefix.length).split(/ +/);
+  const argsArray = args;
   const command = args.shift().toLowerCase();
-
-  if (command === 'ping') {
-    msg.channel.send('pong!');
+  
+  if (command === 'language') {
+    if (argsArray.length === 3) {
+      fetch(xivApi+'/character/search?name='+argsArray[0]+' '+argsArray[1]+'&server='+argsArray[2], {
+        mode: 'cors' 
+      })
+      .then(response => response.json())
+      
+      .then(data => {
+        msg.channel.send(data.Results[0].Lang)
+      })
+    } else {
+      console.log('Name too short');
+      msg.channel.send('Please enter your full information in this format: First Last Server');
+    }
   }
 
   console.log("Message received.");
