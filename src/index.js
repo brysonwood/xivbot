c888740b64a5a107d195be231db5eccf887e1b70
Bryson Wood <mail@brysonwood.com>
2023-09-10 14:47:17 -0700

    Added lodestone search by ID for patch 6.48

--- src/index.js
+++ src/index.js
@@ -41,6 +41,58 @@ function charSearch(charSearchType, charSearchResponse, message, firstName, last
     });
 }
 
+// Started the process of getting the lodestoneSearch function going, I found a solution but it
+// doesn't use the params .get feature of axios that I want it to use.
+//
+// Eventually I'd like to have the functionality to parse all information from here, in addition to by
+// name/server.
+function lodestoneSearch(charSearchType, charSearchResponse, message, lodestoneID) {
+
+  const charSearchy = charSearchResponse;
+  const lodestoneUrl = `${xivApi}/character/${lodestoneID}`;
+
+  axios
+    .get(lodestoneUrl)
+    .then((response) => {
+      if (!response.data || response.data.Character.length === 0) {
+        throw new Error('Name not found.');
+      }
+
+      // Show avatar.
+      const lodestoneAvatar = response.data.Character.Avatar;
+      console.log(`Character Avatar: ${lodestoneAvatar}`);
+      message.channel.send(lodestoneAvatar);
+
+      // Show name.
+      const lodestoneName = response.data.Character.Name;
+      console.log(`Character Name: ${lodestoneName}`);
+      message.channel.send(`Character Name: ${lodestoneName}`);
+
+
+
+      // Show Jobs (Will need to be updated with X.0 releases of FFXIV).
+      const classJobs = response.data.Character.ClassJobs;
+      let foundClassJob = [];
+      let foundClassName = [];
+      let counter = 0;
+
+      // Get class jobs into separate arrays and send a message with the details. (Temporary
+      // until I setup a nice looking display message.) Will save these instead and print them nicely
+      // in the future.
+      for (const classJob of classJobs) {
+        foundClassJob.push(classJob.Level);
+        foundClassName.push(classJob.UnlockedState.Name);
+        console.log(counter);
+        message.channel.send(`${foundClassName[counter]}: lvl${foundClassJob[counter]}`);
+        counter++;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      message.channel.send('Your name was not found in the database.');
+    });
+}
+
 client.on("messageCreate", msg  => {
   if (!msg.content.startsWith(prefix) || msg.author.bot) return;
     
@@ -67,7 +119,11 @@ client.on("messageCreate", msg  => {
     
   } 
 
-  console.log("Message received.");
+  if (command === 'lodestone') {
+    lodestoneSearch('sample', 'sample', msg, argsArray[0]);
+  }
+
+  console.log("Message received.\n----");
 });
 
 client.login(process.env.CLIENT_TOKEN);
